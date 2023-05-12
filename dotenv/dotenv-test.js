d63e4e0c08c650cdc04db889c2486309cb424829
Sarah Cronk <sarah.cronk@digital.cabinet-office.gov.uk>
2023-05-12 08:41:45 +0100

    OJ-1402 linting

--- dotenv/dotenv-test.js
+++ dotenv/dotenv-test.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const dotenv = require('dotenv');
+import * as path from "path";
+import * as dotenv from "dotenv";
 
 module.exports = async () => {
     dotenv.config({ path: path.resolve(__dirname, '../.env.testing') });
