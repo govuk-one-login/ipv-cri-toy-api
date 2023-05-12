81ebd3d4a95e7e44ddf51666e92b651590515c32
Sarah Cronk <sarah.cronk@digital.cabinet-office.gov.uk>
2023-05-12 09:04:12 +0100

    OJ-1402

--- dotenv/dotenv-test.js
+++ dotenv/dotenv-test.js
@@ -1,5 +1,5 @@
-import * as path from "path";
-import * as dotenv from "dotenv";
+const path = require("path");
+const dotenv = require("dotenv");
 
 module.exports = async () => {
     dotenv.config({ path: path.resolve(__dirname, '../.env.testing') });
