fcb3309d2985325a695969d35b78edab5275e599
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2023-05-23 12:39:39 +0100

    chore: Apply pre-commit formatting

--- dotenv/dotenv-test.js
+++ dotenv/dotenv-test.js
@@ -2,5 +2,5 @@ const path = require("path");
 const dotenv = require("dotenv");
 
 module.exports = async () => {
-    dotenv.config({ path: path.resolve(__dirname, '../.env.testing') });
+  dotenv.config({ path: path.resolve(__dirname, "../.env.testing") });
 };
