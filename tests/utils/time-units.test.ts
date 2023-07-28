53dee8a21b32235a7a4b04c251dd4ec30d516960
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-07-28 15:09:07 +0100

    Fix still missing month and year

--- tests/utils/time-units.test.ts
+++ tests/utils/time-units.test.ts
@@ -2,11 +2,15 @@ import { TimeUnit } from "../../lambdas/common/utils/time-units";
 
 describe("TimeUnit", () => {
   describe("convertTo", () => {
-    it("should return the correct conversion for each unit", () => {
-      expect(TimeUnit.convert(TimeUnit.Unit.Seconds)).toBe(1000);
-      expect(TimeUnit.convert(TimeUnit.Unit.Minutes)).toBe(1000 * 60);
-      expect(TimeUnit.convert(TimeUnit.Unit.Hours)).toBe(1000 * 60 * 60);
-      expect(TimeUnit.convert(TimeUnit.Unit.Days)).toBe(1000 * 60 * 60 * 24);
+    it.each([
+      [TimeUnit.Unit.Seconds, 1000],
+      [TimeUnit.Unit.Minutes, 1000 * 60],
+      [TimeUnit.Unit.Hours, 1000 * 60 * 60],
+      [TimeUnit.Unit.Days, 1000 * 60 * 60 * 24],
+      [TimeUnit.Unit.Months, 1000 * 60 * 60 * 24 * 30],
+      [TimeUnit.Unit.Years, 1000 * 60 * 60 * 24 * 365],
+    ])("should convert %s conversion to %s", (unit, expected) => {
+      expect(TimeUnit.convert(unit)).toBe(expected);
     });
 
     it("should throw an error for an unexpected unit", () => {
@@ -17,19 +21,44 @@ describe("TimeUnit", () => {
   });
 
   describe("getValue", () => {
-    it("should return the correct value for each unit", () => {
-      expect(TimeUnit.getValue("seconds")).toBe(TimeUnit.Unit.Seconds);
-      expect(TimeUnit.getValue("minutes")).toBe(TimeUnit.Unit.Minutes);
-      expect(TimeUnit.getValue("hours")).toBe(TimeUnit.Unit.Hours);
-      expect(TimeUnit.getValue("days")).toBe(TimeUnit.Unit.Days);
-      expect(TimeUnit.getValue("months")).toBe(TimeUnit.Unit.Months);
-      expect(TimeUnit.getValue("years")).toBe(TimeUnit.Unit.Years);
+    describe("lowercase", () => {
+      it.each([
+        ["seconds", TimeUnit.Unit.Seconds],
+        ["minutes", TimeUnit.Unit.Minutes],
+        ["hours", TimeUnit.Unit.Hours],
+        ["days", TimeUnit.Unit.Days],
+        ["months", TimeUnit.Unit.Months],
+        ["years", TimeUnit.Unit.Years],
+      ])(
+        "should input '%s' and return the unit as '%s'",
+        (stringUnitInput, expected) => {
+          expect(TimeUnit.getValue(stringUnitInput)).toBe(expected);
+        }
+      );
     });
 
-    it("should throw an error for an invalid unit", () => {
-      expect(() => TimeUnit.getValue("invalid_unit")).toThrowError(
-        "ttlUnit must be valid: invalid_unit"
+    describe("uppercase", () => {
+      it.each([
+        ["SECONDS", TimeUnit.Unit.Seconds],
+        ["MINUTES", TimeUnit.Unit.Minutes],
+        ["HOURS", TimeUnit.Unit.Hours],
+        ["DAYS", TimeUnit.Unit.Days],
+        ["MONTHS", TimeUnit.Unit.Months],
+        ["YEARS", TimeUnit.Unit.Years],
+      ])(
+        "should input '%s' returns the unit as '%s'",
+        (stringUnitInput, expected) => {
+          expect(TimeUnit.getValue(stringUnitInput)).toBe(expected);
+        }
       );
+    });
+
+    describe("invalid unit", () => {
+      it("should throw an error for an invalid unit", () => {
+        expect(() => TimeUnit.getValue("invalid_unit")).toThrowError(
+          "ttlUnit must be valid: invalid_unit"
+        );
+      });
     });
   });
 });
